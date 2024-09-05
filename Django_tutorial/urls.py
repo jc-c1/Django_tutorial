3afd38f25bed2f18a69c6b210bf26a8df1884743
jc-c1 <joannec.chang1@gmail.com>
2024-09-05 15:28:28 -0700

    registration and registration validation added

--- Django_tutorial/urls.py
+++ Django_tutorial/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("", include("travello.urls")),
     # path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
