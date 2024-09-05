246741f6a999a9896962698c169953aa62226729
jc-c1 <joannec.chang1@gmail.com>
2024-09-05 14:34:40 -0700

    added media url

--- Django_tutorial/urls.py
+++ Django_tutorial/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("calc/", include("calc.urls")),
     path("", include("travello.urls")),
     # path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
