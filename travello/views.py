297b0ab5678abc0978a8344429485ba57a03f34e
jc-c1 <joannec.chang1@gmail.com>
2024-09-05 12:24:26 -0700

    dynamic rendering from hardcoded data

--- travello/views.py
+++ travello/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 
 def index(request):
-    return render(request, 'index.html')
+    
+    dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.desc = "Sunny sunny beach"
+    dest1.img = "destination_1.jpg"
+    dest1.price = 360
+    dest1.offer = False
+    
+    
+    dest2 = Destination()
+    dest2.name = "Indonesia"
+    dest2.desc = "Tropical paradise"
+    dest2.img = "destination_2.jpg"
+    dest2.price = 800
+    dest2.offer = True
+    
+    
+    dest3 = Destination()
+    dest3.name = "San Francisco"
+    dest3.desc = "Large red bridge"
+    dest3.img = "destination_3.jpg"
+    dest3.price = 360
+    dest3.offer = False
+    
+    dests = [dest1, dest2, dest3]
+    return render(request, 'index.html', {'dests': dests })
 
