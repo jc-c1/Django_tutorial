ae788f7ceea356dbee3148963790965df42c53cd
jc-c1 <joannec.chang1@gmail.com>
2024-09-05 15:39:33 -0700

    added login

--- accounts/views.py
+++ accounts/views.py
@@ -24,15 +24,25 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
+                return redirect('login')
             
         else:
             messages.info(request, 'Mismatched Passwords')
             return redirect('register')
-            
-        return redirect('/')
     else:
         return render(request, 'register.html')
 
-# def login(request):
-#     return ""
+def login(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, "invalid credentials")
+            return redirect('login')
+    else:
+        return render(request,'login.html')
 
